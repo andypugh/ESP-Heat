0c175f30f68744d3b6e542f0c30a3eae9ea85c48
andypugh <andy@bodgesoc.org>
2020-11-24 22:22:27 +0000

    Create sample credentials.h file
    This will need to be configured to suit your setup. 
    If the error_email is a null string ("\0") then error email sending will be disabled

---
+++ credentials.h
@@ -0,0 +1,8 @@
+// Replace with your network credentials
+const char* ssid = "inser ssid";
+const char* password = "insert the ssid password";
+const char* error_email = "insert email account that will send error messages.";
+const char* error_recipient = "this is the _recipient_ of the error messages";
+const char* error_password = "password for the mail account";
+const char* smtp_server = "insert mail server";
+const int smtp_port = 465;
