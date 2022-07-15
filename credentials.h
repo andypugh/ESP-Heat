4bbbda9a3fe324f2526aca4e8c92cdffb1b264bf
andy pugh <andy@bodgesoc.org>
2022-07-15 16:41:55 +0100

    Add email subject to credentials

--- credentials.h
+++ credentials.h
@@ -3,6 +3,7 @@ const char* ssid = "inser ssid";
 const char* password = "insert the ssid password";
 const char* error_email = "insert email account that will send error messages.";
 const char* error_recipient = "this is the _recipient_ of the error messages";
+const char* error_subject = "ths will be the email title of status reports";
 const char* error_password = "password for the mail account";
 const char* smtp_server = "insert mail server";
 const int smtp_port = 465;
