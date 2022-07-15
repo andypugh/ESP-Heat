a24eddc45500960dfbd215d6ce60d6b14d3bb85c
andy pugh <andy@bodgesoc.org>
2022-07-15 17:31:37 +0100

    add http authentication data to credentials

--- credentials.h
+++ credentials.h
@@ -1,5 +1,5 @@
 // Replace with your network credentials
-const char* ssid = "inser ssid";
+const char* ssid = "insert ssid";
 const char* password = "insert the ssid password";
 const char* error_email = "insert email account that will send error messages.";
 const char* error_recipient = "this is the _recipient_ of the error messages";
@@ -7,3 +7,6 @@ const char* error_subject = "ths will be the email title of status reports";
 const char* error_password = "password for the mail account";
 const char* smtp_server = "insert mail server";
 const int smtp_port = 465;
+const char* http_user = "admin";
+const char* http_pass = "admin";
+
